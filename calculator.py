564386759a987a7930e5fedc767b83843dfb9228
Salisu S. Muhd <cyieph@gmail.com>
2026-07-01 10:53:51 +0100

    Add multiplication and division functions
    Added multiplication and division functions with a typo in the division function.

--- calculator.py
+++ calculator.py
@@ -2,5 +2,9 @@ def add(a, b):
   return a + b
 def subtract(a, b):
   return a - b
+def multiplication (a, b):
+    return a*b
+def division (a, b):
+  retun a/b
 print("Addition:", add(5, 3))
 print("Subtraction:", subtract(5, 3))
